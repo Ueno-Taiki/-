2320f63438b07aba572ecce560613bdd49787628
上野太己 <k023g0051@g.neec.ac.jp>
2024-10-09 14:24:18 +0900

    基礎構造

---
+++ Enemy.h
@@ -0,0 +1,24 @@
+#pragma once
+
+class Enemy
+{
+public:
+	/// </summary>
+	/// 初期化
+	/// </summary>
+	void Initialize();
+
+	/// </summary>
+	/// 初期化
+	/// </summary>
+	void Update();
+
+	/// </summary>
+	/// 初期化
+	/// </summary>
+	void Draw();
+
+private:
+
+};
+
---
+++ Player.cpp
@@ -0,0 +1,16 @@
+#include "Player.h"
+
+void Player::Initialize()
+{
+
+}
+
+void Player::Update()
+{
+
+}
+
+void Player::Draw()
+{
+
+}
